02e99f79bf9f5251f100e2d76c8b007f67f76aca
Eimantas Noreika <noreika.eimantas@gmail.com>
2022-11-02 10:05:43 +0000

    Release for commit: Moved User Update Cmd To User Controller

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -63,6 +63,53 @@ class UserApi {
     return null;
   }
 
+  /// Performs an HTTP 'POST /api/User/Update' operation and returns the [Response].
+  /// Parameters:
+  ///
+  /// * [UpdateInfoCmd] updateInfoCmd (required):
+  Future<Response> userUpdateWithHttpInfo(UpdateInfoCmd updateInfoCmd,) async {
+    // ignore: prefer_const_declarations
+    final path = r'/api/User/Update';
+
+    // ignore: prefer_final_locals
+    Object? postBody = updateInfoCmd;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>['application/json'];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'POST',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// Parameters:
+  ///
+  /// * [UpdateInfoCmd] updateInfoCmd (required):
+  Future<bool?> userUpdate(UpdateInfoCmd updateInfoCmd,) async {
+    final response = await userUpdateWithHttpInfo(updateInfoCmd,);
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'bool',) as bool;
+    
+    }
+    return null;
+  }
+
   /// Performs an HTTP 'GET /api/User/UserInfo' operation and returns the [Response].
   Future<Response> userUserInfoWithHttpInfo() async {
     // ignore: prefer_const_declarations
